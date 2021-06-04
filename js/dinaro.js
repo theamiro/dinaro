c8729eecee8a0197f6215a4ea10a6ac480c3f928
Michael Amiro <odindo.michael@gmail.com>
2021-06-04 09:04:07 +0300

    Added How it works. Cleaned responsiveness. Fixed Navbar.

--- js/dinaro.js
+++ js/dinaro.js
@@ -12060,6 +12060,18 @@ $(document).ready(function () {
 		checkVisibility()
 	})
 })
+
+$(document).on("click", 'a[href^="#"]', function (event) {
+	event.preventDefault()
+
+	$("html, body").animate(
+		{
+			scrollTop: $($.attr(this, "href")).offset().top,
+		},
+		500
+	)
+})
+
 function checkVisibility() {
 	$body = $("body")
 	var e = $(window).height(),
